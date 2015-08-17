7f9234525e6af3b96f6a188ba1b1388a8e6a297a
Barry Anders <barry@netforger.com>
2015-08-17 14:31:28 -0500

    Added Cordova and made QT5 optional.

--- prime/dev.sh
+++ prime/dev.sh
@@ -1,5 +1,5 @@
 # Permissions
-echo ; echo '---Permissions' ; echo
+echo ; echo '--- Permissions' ; echo
 echo 'Granting Executive Rights...'
 sudo chown $(whoami):admin /usr/local/
 sudo chown $(whoami):admin /usr/local/*
@@ -19,8 +19,8 @@ sudo gem update --system
 echo ; read -p 'Install Gem: Jekyll? (y/n) ' answer
 case ${answer:0:1} in
   y|Y )
-    echo 'Installing Jekyll...'
-    sudo gem install -n /usr/local/bin jekyll
+  echo 'Installing Jekyll...'
+  sudo gem install -n /usr/local/bin jekyll
   ;;
 esac
 
@@ -35,27 +35,35 @@ echo ; echo '--- Development Packages' ; echo
 read -p 'Install Development Packages? (y/n) ' answer
 case ${answer:0:1} in
   y|Y )
-    # Ack
-    echo ; echo 'Installing Ack...'
-    brew install ack
-    # GCC
-    echo 'Installing GCC...'
-    brew install gcc
-    # QT5
+  # Ack
+  echo ; echo 'Installing Ack...'
+  brew install ack
+  # GCC
+  echo ; echo 'Installing GCC...'
+  brew install gcc
+  # QT5
+  echo ; read -p 'Install QT5? (y/n) ' answer
+  case ${answer:0:1} in
+  y|Y )
     echo 'Installing QT5...'
     brew install qt5 --with-docs --with-developer --with-d-bus --with-mysql
-    # Node
-    echo ; echo 'Installing Node...'
-    brew install node
-    echo ; echo 'Upgrading Homebrew Packages...'
-    brew upgrade
-    # Underscore
-    echo ; echo 'Installing Underscore...'
-    sudo npm install -g underscore
-    # Moment
-    echo 'Installing Moment...'
-    sudo npm install -g moment
-    echo ; echo 'Updating Node Packages...'
-    npm update
+    ;;
+  esac
+  # Node
+  echo ; echo 'Installing Node...'
+  brew install node
+  echo ; echo 'Upgrading Homebrew Packages...'
+  brew upgrade
+  # Underscore
+  echo ; echo 'Installing Underscore...'
+  sudo npm install -g underscore
+  # Moment
+  echo 'Installing Moment...'
+  sudo npm install -g moment
+  # Cordova
+  echo 'Installing Cordova...'
+  sudo npm install -g cordova
+  echo ; echo 'Updating Node Packages...'
+  npm update
   ;;
 esac

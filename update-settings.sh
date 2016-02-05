37f7205e3d9498cdb107f7c181386c1cb3747548
Barry Anders <barry@netforger.com>
2016-02-05 11:33:45 -0600

    Make update-settings work properly.

---
+++ update-settings.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+cp -Rfv ~/.bash_profile ~/code/\+github/osx-prime/Settings/
+cp -Rfv ~/.gitconfig ~/code/\+github/osx-prime/Settings/
+cp -Rfv ~/.gitignore ~/code/\+github/osx-prime/Settings/
--- update.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-ditto ~/.bash_profile ~/code/+github/osx-prime/
-ditto ~/.gitconfig ~/code/+github/osx-prime/
-ditto ~/.gitignore ~/code/+github/osx-prime/

304261220f8255c3c60aa3e7b840fb51a1669075
Barry Anders <barry@netforger.com>
2016-02-22 10:10:53 -0600

    Add .gemrc to the list of dot configs.

--- update-settings.sh
+++ update-settings.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 cp -Rfv ~/.bash_profile ~/code/\+github/osx-prime/Settings/
+cp -Rfv ~/.gemrc ~/code/\+github/osx-prime/Settings/
 cp -Rfv ~/.gitconfig ~/code/\+github/osx-prime/Settings/
 cp -Rfv ~/.gitignore ~/code/\+github/osx-prime/Settings/

63f03063adbc895ce3860004b780c2f2f4c6f9ac
Barry <5648875+BarryMode@users.noreply.github.com>
2017-07-26 00:21:01 -0500

    npm.sh

---
+++ applications/npm.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+sudo -v
+
+npm install -g lazysizes
+npm install -g lodash
+npm install -g moment
+npm install -g nativefier
+npm install -g oboe
+
+nativefier --disable-context-menu --disable-dev-tools --name "Trello" --icon "../icons/Trello.icns" "https://trello.com" "/Applications"

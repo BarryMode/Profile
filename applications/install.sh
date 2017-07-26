a37dcd34c1fa15376515b7fb70356f063ce7e249
Barry <5648875+BarryMode@users.noreply.github.com>
2017-07-26 00:21:55 -0500

    applications/install.sh

---
+++ applications/install.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+cd ~/prime/applications/
+
+# Brew - The missing package manager for macOS
+# http://brew.sh
+chmod 755 brew.sh
+./brew.sh
+
+# RubyGems - a package management framework for Ruby
+# https://rubygems.org
+chmod 755 gem.sh
+./gem.sh
+
+# NPM - the package manager for JavaScript
+# https://www.npmjs.com
+chmod 755 npm.sh
+./npm.sh

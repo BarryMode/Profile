aced2482bc927af1db380d0f17a67e1b07f6bd0e
Barry <5648875+BarryMode@users.noreply.github.com>
2017-07-28 21:33:50 -0500

    mas.sh: nice collection of Electron guides

--- applications/mas.sh
+++ applications/mas.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 mas install 420212497  # Byword
+mas install 1119345146 # Electron APIs
 mas install 905384638  # FileBot
 mas install 1179623856 # Pastebot
 mas install 715768417  # Microsoft Remote Desktop

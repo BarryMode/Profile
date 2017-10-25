088af4735205c809eb06b7ecfefee8995834de33
Barry <5648875+BarryMode@users.noreply.github.com>
2017-10-25 10:36:44 -0500

    Remove Pastebot

--- applications/mas.sh
+++ applications/mas.sh
@@ -3,7 +3,6 @@
 mas install 420212497  # Byword
 mas install 1119345146 # Electron APIs
 mas install 905384638  # FileBot
-mas install 1179623856 # Pastebot
 mas install 715768417  # Microsoft Remote Desktop
 mas install 1176895641 # Spark
 mas install 1278508951 # Trello

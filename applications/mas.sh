cc26cd507af8ff31baf358fdc9a67d4985b73a9d
Barry <5648875+BarryMode@users.noreply.github.com>
2018-08-02 00:46:18 -0500

    Reverted to Gmail
    I'm done with Spark.

--- applications/mas.sh
+++ applications/mas.sh
@@ -5,5 +5,4 @@ mas install 1119345146 # Electron APIs
 mas install 905384638  # FileBot
 mas install 668208984  # GIPHY
 mas install 715768417  # Microsoft Remote Desktop
-mas install 1176895641 # Spark
 mas install 497799835  # Xcode

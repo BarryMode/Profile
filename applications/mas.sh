e1ac8da395d99ebc5edfe49ecafcc02dffbe3459
Barry <5648875+BarryMode@users.noreply.github.com>
2018-08-30 15:25:43 -0500

    Too many spaces

--- applications/mas.sh
+++ applications/mas.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-mas install 420212497   # Byword
-mas install 905384638   # FileBot
-mas install 668208984   # GIPHY
-mas install 1295203466  # Microsoft Remote Desktop
-mas install 497799835   # Xcode
+mas install 420212497  # Byword
+mas install 905384638  # FileBot
+mas install 668208984  # GIPHY
+mas install 1295203466 # Microsoft Remote Desktop
+mas install 497799835  # Xcode

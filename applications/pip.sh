d1c398dd9291941e3ffd6bcd9560bff7451e754c
Barry <5648875+BarryMode@users.noreply.github.com>
2018-08-30 15:25:23 -0500

    Don't need mincss, Gulp is better

--- applications/pip.sh
+++ applications/pip.sh
@@ -5,7 +5,3 @@ sudo easy_install pip
 
 # Make sure we're using the latest PIP
 pip install --upgrade pip
-
-# mincss
-# https://github.com/peterbe/mincss
-sudo pip install --ignore-installed mincss

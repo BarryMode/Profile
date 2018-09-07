9fc88e1f2ceee64d15ab43334a2a3d03a7de0d3a
Barry <5648875+BarryMode@users.noreply.github.com>
2018-09-07 10:32:58 -0500

    mas: add unsplash wallpapers

--- applications/mas.sh
+++ applications/mas.sh
@@ -4,4 +4,5 @@ mas install 420212497  # Byword
 mas install 905384638  # FileBot
 mas install 668208984  # GIPHY
 mas install 1295203466 # Microsoft Remote Desktop
+mas install 1284863847 # Unsplash Wallpapers
 mas install 497799835  # Xcode

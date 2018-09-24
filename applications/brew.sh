9eb396a72be9ff387f4444157701c10902b2011d
Barry <5648875+BarryMode@users.noreply.github.com>
2018-09-24 14:30:42 -0500

    MAMP because it's just easier sometimes

--- applications/brew.sh
+++ applications/brew.sh
@@ -110,6 +110,7 @@ brew cask install jdownloader
 brew cask install keka
 brew cask install keybase
 brew cask install lackeyccg
+brew cask install mamp
 brew cask install obs
 brew cask install openemu
 brew cask install paparazzi

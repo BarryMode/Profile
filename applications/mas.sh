b619711a0848a7321cd97f81ec58aa30aba47d5f
Barry <5648875+BarryMode@users.noreply.github.com>
2018-09-25 11:43:35 -0500

    Fun, but definitely not one to keep long term

--- applications/mas.sh
+++ applications/mas.sh
@@ -3,5 +3,4 @@
 mas install 420212497  # Byword
 mas install 668208984  # GIPHY
 mas install 1295203466 # Microsoft Remote Desktop
-mas install 1284863847 # Unsplash Wallpapers
 mas install 497799835  # Xcode

359f475eb170bfdac444c241e5106444120d2c9c
Barry <5648875+BarryMode@users.noreply.github.com>
2018-10-06 17:35:36 -0500

    These modules don't need to be installed globally

--- applications/npm.sh
+++ applications/npm.sh
@@ -6,10 +6,6 @@ npm install -g npm@latest
 # Update already installed Node packages
 npm update -g
 
-npm install -g crypto-js
 npm install -g ghost-cli
-npm install -g gulp
 npm install -g lodash
 npm install -g moment
-npm install -g oboe
-npm install -g plex-dl

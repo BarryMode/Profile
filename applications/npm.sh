532220833961c0f684083f0618a70b430fef6020
Barry <5648875+BarryMode@users.noreply.github.com>
2018-10-22 19:37:23 -0500

    npm.sh: add angular cli

--- applications/npm.sh
+++ applications/npm.sh
@@ -6,6 +6,7 @@ npm install -g npm@latest
 # Update already installed Node packages
 npm update -g
 
+npm install -g @angular/cli
 npm install -g ghost-cli
 npm install -g gulp-cli
 npm install -g ionic

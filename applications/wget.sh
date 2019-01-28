33f6c61673a0583b70dfc1ec7fe8bd3a87303f6f
Barry <5648875+BarryMode@users.noreply.github.com>
2019-01-28 10:58:53 -0600

    ProtonVPN is in brew cask

--- applications/wget.sh
+++ applications/wget.sh
@@ -4,6 +4,3 @@ WGET_DL_DIR='~/downloads/'
 
 # iShowU Audio Capture
 wget -P $WGET_DL_DIR https://downloads.shinywhitebox.com/driver/latest/
-
-# ProtonVPN
-wget -P $WGET_DL_DIR https://protonvpn.com/download/ProtonVPN.dmg

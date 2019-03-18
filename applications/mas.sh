a6949f8eb6a442dded7dfc0d63dbf82d35c19ce7
Barry <5648875+BarryMode@users.noreply.github.com>
2019-03-18 10:50:34 -0500

    The Unarchiver added as a backup (now that it is owned by MacPaw)

--- applications/mas.sh
+++ applications/mas.sh
@@ -3,4 +3,5 @@
 mas install 420212497  # Byword
 mas install 668208984  # GIPHY
 mas install 1295203466 # Microsoft Remote Desktop
+mas install 425424353  # The Unarchiver
 mas install 497799835  # Xcode

1bfbf3926ea3d06c1797b39f612862fbc30edbe7
Barry <5648875+BarryMode@users.noreply.github.com>
2019-03-19 11:39:54 -0500

    mas.sh: PiPer for Safari

--- applications/mas.sh
+++ applications/mas.sh
@@ -3,5 +3,6 @@
 mas install 420212497  # Byword
 mas install 668208984  # GIPHY
 mas install 1295203466 # Microsoft Remote Desktop
+mas install 1421915518 # PiPer
 mas install 425424353  # The Unarchiver
 mas install 497799835  # Xcode

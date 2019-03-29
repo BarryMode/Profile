f1bd004887cf95ad2f90085bcb4484427465b896
Barry <5648875+BarryMode@users.noreply.github.com>
2019-03-29 09:06:58 -0500

    brew.sh: homebrew-php is deprecated

--- applications/brew.sh
+++ applications/brew.sh
@@ -17,7 +17,6 @@ brew tap homebrew/services
 brew tap caskroom/cask
 brew tap caskroom/fonts
 brew tap buo/cask-upgrade
-brew tap josegonzalez/homebrew-php
 
 # Install GNU core utilities (those that come with macOS are outdated)
 # Don’t forget to add `$(brew --prefix coreutils)/libexec/gnubin` to `$PATH`

a2e236aa537d692bfb9b52bdbbc87bf78c62fbc9
Barry <5648875+BarryMode@users.noreply.github.com>
2019-04-09 15:12:43 -0500

    hosts: experimenting with blocking more sites

--- hosts/install.sh
+++ hosts/install.sh
@@ -1,3 +1,3 @@
-python ~/prime/hosts/generate.py --auto --replace
+python ~/prime/hosts/generate.py --auto --replace --extensions gambling porn fakenews
 cp ~/prime/hosts/httpd-vhosts.conf /Applications/MAMP/conf/apache/extra/httpd-vhosts.conf
 rm -rf ~/prime/hosts/hosts

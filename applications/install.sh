56a699f50fc89526665b87a107432b99e5a5abd4
Barry <5648875+BarryMode@users.noreply.github.com>
2019-04-09 15:07:30 -0500

    applications: comment updates

--- applications/install.sh
+++ applications/install.sh
@@ -3,27 +3,27 @@
 cd ~/prime/applications/
 chmod +x *.sh
 
-# Brew: The missing package manager for macOS
+# Brew - The missing package manager for macOS
 # https://github.com/Homebrew/brew
 ./brew.sh
 
-# MAS: Mac App Store command line interface
+# MAS - Mac App Store command line interface
 # https://github.com/mas-cli/mas
 ./mas.sh
 
-# Wget: a computer program that retrieves content from web servers
+# Wget - A computer program that retrieves content from web servers
 # https://github.com/mirror/wget
 ./wget.sh
 
-# Composer: dependency manager for PHP
+# Composer - Dependency Manager for PHP
 # https://github.com/composer/composer
 ./composer.sh
 
-# RubyGems: a package management framework for Ruby
+# RubyGems: A package management framework for Ruby
 # https://github.com/rubygems/rubygems
 ./gem.sh
 
-# NPM: the package manager for JavaScript
+# NPM: The package manager for JavaScript
 # https://github.com/npm/cli
 ./npm.sh
 
@@ -31,6 +31,6 @@ chmod +x *.sh
 # https://github.com/pypa/pip
 ./pip.sh
 
-# Yarn: fast, reliable, and secure dependency management
+# Yarn: Fast, reliable, and secure dependency management
 # https://github.com/yarnpkg/yarn
 ./yarn.sh

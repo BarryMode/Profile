29ae086f70775e7de7eb04e9a67ce70cb645185a
Barry <5648875+BarryMode@users.noreply.github.com>
2019-04-11 10:33:44 -0500

    brew.sh: `--with-override-system-vi` is the default behavior

--- applications/brew.sh
+++ applications/brew.sh
@@ -47,7 +47,7 @@ brew install wget
 brew install gpg pinentry-mac
 
 # Install more recent versions of some macOS tools.
-brew install vim --with-override-system-vi
+brew install vim
 brew install grep
 brew install openssh
 brew install screen

9eae78975e0be5777d002e2a4df8b711862f1fdb
Barry <5648875+BarryMode@users.noreply.github.com>
2019-04-11 10:31:43 -0500

    Formatting

--- applications/brew.sh
+++ applications/brew.sh
@@ -3,7 +3,7 @@
 # Install Homebrew
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
-# Make sure we’re using the latest Homebrew.
+# Make sure we're using the latest Homebrew.
 brew update
 
 # Upgrade any already-installed formulae.

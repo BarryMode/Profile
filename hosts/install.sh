77a91a71f9272e1ff023b43912b356c3c5b8cff0
Barry <5648875+BarryMode@users.noreply.github.com>
2019-04-11 23:25:44 -0500

    hosts: sort extensions alphabetically

--- hosts/install.sh
+++ hosts/install.sh
@@ -1,3 +1,3 @@
-python ~/prime/hosts/generate.py --auto --replace --extensions gambling porn fakenews
+python ~/prime/hosts/generate.py --auto --replace --extensions fakenews gambling porn
 cp ~/prime/hosts/httpd-vhosts.conf /Applications/MAMP/conf/apache/extra/httpd-vhosts.conf
 rm -rf ~/prime/hosts/hosts

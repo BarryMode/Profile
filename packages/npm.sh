58f1e0a4e48d1ed4bc0e992a78ee3bb5a5a2d365
Barry <5648875+BarryMode@users.noreply.github.com>
2019-05-17 09:12:31 -0500

    Move ionic and cordova to yarn

--- packages/npm.sh
+++ packages/npm.sh
@@ -6,5 +6,3 @@ npm install -g npm
 # Update already installed Node packages
 npm update -g
 
-npm install -g cordova # for building with ionic (at least v1)
-npm install -g ionic
--- packages/yarn.sh
+++ packages/yarn.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
 
+yarn global add cordova
 yarn global add corsproxy-https
 yarn global add ghost-cli
 yarn global add gulp-cli
+yarn global add ionic
 yarn global add lodash
 yarn global add moment
 yarn global add video.js

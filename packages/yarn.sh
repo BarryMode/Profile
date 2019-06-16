8bfa145d8f2c2ed85d8be7b7d327535569dd2934
Barry <5648875+BarryMode@users.noreply.github.com>
2019-06-15 23:44:49 -0500

    Switch yarn back to npm

--- packages/yarn.sh
+++ packages/yarn.sh
@@ -1,9 +1,1 @@
 #!/usr/bin/env bash
-
-yarn global add cordova
-yarn global add corsproxy-https
-yarn global add ghost-cli
-yarn global add ionic
-yarn global add lodash
-yarn global add moment
-yarn global add video.js

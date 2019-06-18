81d43f1c765ffe09285995723a416658c5ef7151
Barry <5648875+BarryMode@users.noreply.github.com>
2019-06-18 11:16:19 -0500

    brew.sh: remove vanilla

--- packages/brew.sh
+++ packages/brew.sh
@@ -141,7 +141,6 @@ brew cask install steam
 brew cask install sublime-text
 brew cask install teamviewer
 brew cask install telegram
-brew cask install vanilla
 brew cask install virtualbox && brew cask install local-by-flywheel
 brew cask install vlc
 brew cask install webtorrent

8da7d6806729193553095aed897106327c164552
Barry <5648875+BarryMode@users.noreply.github.com>
2020-11-14 21:15:46 -0600

    Symlinks update

--- update:symlinks.sh
+++ update:symlinks.sh
@@ -6,12 +6,12 @@
 if [ -d "/Volumes/GoogleDrive" ]; then
   for dir in Applications Books Documents Documents/Computers Games Music Pictures Projects Projects/YouTube Projects/YouTube/Assets Projects/YouTube/gsc; do
     #------------------------------------------------------------------------------------#
-    # Applications/Documents/Games/Music/Pictures/Projects: find type symlink and remove
+    # Documents/Games/Music/Pictures/Projects: find type symlink and remove
     #------------------------------------------------------------------------------------#
     cd ~/$dir
     fd --type l --exec rm
     #-----------------------------------------------------------------------------------------------#
-    # Applications/Documents/Games/Music/Pictures/Projects: find type directory and create symlinks
+    # Documents/Games/Music/Pictures/Projects: find type directory and create symlinks
     #-----------------------------------------------------------------------------------------------#
     cd "/Volumes/GoogleDrive/My Drive/$dir"
     fd --type d --max-depth 1 --exec ln -s "$(pwd)/{}" ~/$dir
@@ -22,14 +22,14 @@ fi
 # Adler Symlinks
 #==============================================================================#
 if [ -d "/Volumes/Adler" ]; then
-  for dir in Documents Documents/Computers Games Pictures; do
+  for dir in Documents Documents/Computers Pictures; do
     #--------------------------------------------------------#
-    # Documents/Games/Pictures: find type symlink and remove
+    # Documents/Pictures: find type symlink and remove
     #--------------------------------------------------------#
     # cd ~/$dir
     # fd --type l --exec rm
     #-------------------------------------------------------------------#
-    # Documents/Games/Pictures: find type directory and create symlinks
+    # Documents/Pictures: find type directory and create symlinks
     #-------------------------------------------------------------------#
     cd "/Volumes/Adler/$dir"
     fd --type d --max-depth 1 --exec ln -s "$(pwd)/{}" ~/$dir
